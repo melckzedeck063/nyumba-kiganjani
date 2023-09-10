f5721ed3af1773e94111709f671e934d84564e5a
melckzedeck <melckzedeck063@gmail.com>
2023-09-10 23:59:54 +0300

    modified register and login activity

--- app/src/main/java/com/example/dkiganjani/HomeActivity.java
+++ app/src/main/java/com/example/dkiganjani/HomeActivity.java
@@ -1,34 +1,85 @@
 package com.example.dkiganjani;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private PropertyAdapter propertyAdapter;
-    ArrayList<Property>  propertyArrayList;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private FrameLayout frameLayout;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
-        recyclerView = findViewById(R.id.recycle_view1);
+
+       frameLayout = findViewById(R.id.frame_container);
+        toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar); // Set the Toolbar as the ActionBar
 
 
-//        createProperty();
+        navigationView =  findViewById(R.id.nav_menu);
+        drawerLayout =  findViewById(R.id.drawer_container);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(HomeActivity.this, drawerLayout, toolbar, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.user_menu:
+//                            drawerLayout.closeDrawer(GravityCompat.START);
+                                startActivity(new Intent(HomeActivity.this, NewPropActivity.class));
+                            break;
+                        case R.id.settings_menu:
+//                                drawerLayout.closeDrawer(GravityCompat.START);
+                                Toast.makeText(HomeActivity.this, "SETTINGS MENU CALLED", Toast.LENGTH_SHORT).show();
+                            break;
+                            // Add more cases for other menu items
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + item.getItemId());
+                    }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+        });
 
         replaceFragment(new HomeFragment());
 
@@ -57,16 +108,6 @@ public class HomeActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    private void createProperty(){
-        propertyArrayList = new ArrayList<>();
-        propertyArrayList.add(new Property("120K/Month", "Self contained room", "Nzuguni Dodoma", "3/6  Months", "", R.mipmap.house1));
-        propertyArrayList.add(new Property("120K/Month", "Self contained room", "Nzuguni Dodoma", "3/6  Months", "", R.mipmap.house2));
-        propertyArrayList.add(new Property("120K/Month", "Self contained room", "Nzuguni Dodoma", "3/6  Months", "", R.mipmap.house3));
-        propertyArrayList.add(new Property("120K/Month", "Self contained room", "Nzuguni Dodoma", "3/6  Months", "", R.mipmap.house1));
-        propertyArrayList.add(new Property("120K/Month", "Self contained room", "Nzuguni Dodoma", "3/6  Months", "", R.mipmap.house3));
 
-        propertyAdapter = new PropertyAdapter(propertyArrayList, getBaseContext());
-        recyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext(), LinearLayoutManager.VERTICAL,false));
-        recyclerView.setNestedScrollingEnabled(false);
-    }
+
 }
--- app/src/main/java/com/example/dkiganjani/HomeFragment.java
+++ app/src/main/java/com/example/dkiganjani/HomeFragment.java
@@ -1,17 +1,32 @@
 package com.example.dkiganjani;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -23,6 +38,10 @@ public class HomeFragment extends Fragment {
     private SharedPreferenceHelper sharedPreferenceHelper;
     private SharedPreferences sharedPreferences;
     TextView user_name;
+    FrameLayout frameLayout;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +49,50 @@ public class HomeFragment extends Fragment {
 
         View view  =   inflater.inflate(R.layout.fragment_home, container, false);
         recyclerView = view.findViewById(R.id.recycler_view2);
+
+
+//        frameLayout = view.findViewById(R.id.frame_layout);
+//        toolbar = view.findViewById(R.id.toolbar);
+//        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar); // Set the Toolbar as the ActionBar
+//
+//
+//        navigationView =  view.findViewById(R.id.nav);
+//        drawerLayout =  view.findViewById(R.id.drawer_layout);
+//
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.open, R.string.close);
+//
+//        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+//        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+//
+//
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                    switch (item.getItemId()) {
+//                        case R.id.user_menu:
+//                            try {
+//                                startActivity(new Intent(getActivity(), NewPropActivity.class));
+//                            } catch (Exception e) {
+//                                Log.e("Navigation Error", "Failed to start NewProperty activity: " + e.getMessage());
+//                            }
+//                            break;
+//                        case R.id.settings_menu:
+//                            try {
+//                                Toast.makeText(getContext(), "SETTINGS MENU CALLED", Toast.LENGTH_SHORT).show();
+//                            } catch (Exception e) {
+//                                Log.e("Navigation Error", "Failed to start NewProperty activity: " + e.getMessage());
+//                            }
+//                            break;
+//                            // Add more cases for other menu items
+//                        default:
+//                            throw new IllegalStateException("Unexpected value: " + item.getItemId());
+//                    }
+//                    drawerLayout.closeDrawer(GravityCompat.START); // Close the drawer
+//                    return true;
+//                }
+//        });
+
 
         sharedPreferenceHelper =  new SharedPreferenceHelper(getContext());
 
@@ -39,8 +102,6 @@ public class HomeFragment extends Fragment {
             Intent intent =  new Intent(getActivity(), LoginActivity.class);
             startActivity(intent);
         }
-        user_name = view.findViewById(R.id.user_name);
-        user_name.setText(firstname);
 
         createProperty();
 
@@ -50,7 +111,7 @@ public class HomeFragment extends Fragment {
 
     private void createProperty(){
         propertyArrayList =  new ArrayList<>();
-        propertyArrayList.add(new Property("280/Month", "3 Bedrooms house", "Kilimani Dodoma", "3/6 Months",  "", R.mipmap.house2));
+        propertyArrayList.add(new Property("280K/Month", "3 Bedrooms house", "Kilimani Dodoma", "3/6 Months",  "", R.mipmap.house2));
         propertyArrayList.add(new Property("120K/Month", "Self contained room", "Nzuguni Dodoma", "3/6  Months", "", R.mipmap.house1));
         propertyArrayList.add(new Property("120K/Month", "Self contained room", "Nzuguni Dodoma", "3/6  Months", "", R.mipmap.house2));
         propertyArrayList.add(new Property("120K/Month", "Self contained room", "Nzuguni Dodoma", "3/6  Months", "", R.mipmap.house3));
@@ -60,5 +121,15 @@ public class HomeFragment extends Fragment {
         propertyAdapter  =  new PropertyAdapter(propertyArrayList, getContext());
         recyclerView.setAdapter(propertyAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    private void navigateActivity(Class<? extends Activity> activityClass) {
+        Log.d("Navigation", "Navigating to " + activityClass.getSimpleName());
+        Intent intent = new Intent(getContext(), activityClass);
+        startActivity(intent);
+    }
+
+    public void displayMessage(){
+        Toast.makeText(getContext(), "Hello nav", Toast.LENGTH_LONG).show();
     }
 }
